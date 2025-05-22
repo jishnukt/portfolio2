6e79d06373f0832d799c1965347ae36d986ef68f
jishnukt <jsn18j@gmail.com>
2025-05-22 23:24:03 +0530

    Error page 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./pages/App.jsx";
-import ErrorPage from "./pages/ErrorPage.jsx";
+// import ErrorPage from "./pages/ErrorPage.jsx";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter basename="/Portfolio"> {/* ✅ Must match repo name */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
