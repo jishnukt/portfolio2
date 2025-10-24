6ab608ae840c80a671830cda1b3735a4d2c6b1b0
JISHNU <jishnu@KAL-EL.local>
2025-10-25 02:54:21 +0530

    second

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,42 +1,42 @@
-// import React, { useState } from 'react'
-// import { SocialIcon } from 'react-social-icons'
-// import { motion } from 'framer-motion'
-// import { ToastContainer, toast } from 'react-toastify'
-// import 'react-toastify/dist/ReactToastify.css';
-// import axios from 'axios'
-// function contact() {
-//     return (
-//         <motion.div initial={{ opacity: 0, x: 40 }}
-//             animate={{ opacity: 1, x: 0 }}
-//             exit={{ opacity: 0, y: 40 }}
-//             transition={{ duration: 0.6 }}
-//             className='flex  flex-col items-center justify-center px-[8vw]'>
-//             <h1 className='heading'>Contact</h1>
-//             <div>
-//                 <ToastContainer position='top-center' autoClose={2000} />
+import React, { useState } from 'react'
+import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios'
+function contact() {
+    return (
+        <motion.div initial={{ opacity: 0, x: 40 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, y: 40 }}
+            transition={{ duration: 0.6 }}
+            className='flex  flex-col items-center justify-center px-[8vw]'>
+            <h1 className='heading'>Contact</h1>
+            <div>
+                <ToastContainer position='top-center' autoClose={2000} />
 
-//                 <div className='grid md:grid-cols-6 grid-cols-3 py-16 gap-10 place-items-center'>
-//                     {[
-//                         "https://facebook.com/jishnu.kt.33",
-//                         "https://twitter.com/jsn18j",
-//                         "https://www.linkedin.com/in/jishnu-kt/",
-//                         "https://instagram.com/jsn_18",
-//                         "https://github.com/jishnukt",
-//                         "mailto:jsn18j@gmail.com"
-//                     ].map((url, index) => (
-//                         <motion.div
-//                             key={index}
-//                             whileHover={{ scale: 1.2, rotate: 5 }}
-//                             whileTap={{ scale: 0.9 }}
-//                             transition={{ type: "spring", stiffness: 300 }}
-//                         >
-//                             <SocialIcon url={url} />
-//                         </motion.div>
-//                     ))}
-//                 </div>
-//             </div>
-//         </motion.div>
-//     )
-// }
+                <div className='grid md:grid-cols-6 grid-cols-3 py-16 gap-10 place-items-center'>
+                    {[
+                        "https://facebook.com/jishnu.kt.33",
+                        "https://twitter.com/jsn18j",
+                        "https://www.linkedin.com/in/jishnu-kt/",
+                        "https://instagram.com/jsn_18",
+                        "https://github.com/jishnukt",
+                        "mailto:jsn18j@gmail.com"
+                    ].map((url, index) => (
+                        <motion.div
+                            key={index}
+                            whileHover={{ scale: 1.2, rotate: 5 }}
+                            whileTap={{ scale: 0.9 }}
+                            transition={{ type: "spring", stiffness: 300 }}
+                        >
+                            <SocialIcon url={url} />
+                        </motion.div>
+                    ))}
+                </div>
+            </div>
+        </motion.div>
+    )
+}
 
-// export default contact
+export default contact
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,19 +1,19 @@
-// import React from 'react'
-// import Nav from '../components/nav/nav'
-// import Education from '../components/education'
-// import Project from '../components/project'
-// import Tech from '../components/tech'
-// import { motion } from 'framer-motion'
-// function resume() {
-//   return (
-//     <motion.div initial={{ opacity: 0, x: -40 }}
-//       animate={{ opacity: 1, x: 0 }}
-//       transition={{ duration: 0.6 }}>
-//       <Education />
-//       <Project />
-//       <Tech/>
-//     </motion.div>
-//   )
-// }
+import React from 'react'
+import Nav from '../components/nav/nav'
+import Education from '../components/education'
+import Project from '../components/project'
+import Tech from '../components/tech'
+import { motion } from 'framer-motion'
+function resume() {
+  return (
+    <motion.div initial={{ opacity: 0, x: -40 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6 }}>
+      <Education />
+      <Project />
+      <Tech/>
+    </motion.div>
+  )
+}
 
-// export default resume
+export default resume
